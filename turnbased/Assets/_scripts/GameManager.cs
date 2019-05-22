986de901e4b64147ca5e94bfcf85615798f30b6e
Kara Thrash <dvdonato@gmail.com>
2019-05-22 14:30:54 -0400

    3rd person and tab to change player

---
+++ turnbased/Assets/_scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cam;
+    public List<Soldier> team0, team1;
+    public int activeTeam, soldierInList;
+    public Soldier selected;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) { ChangeFocus(); }
+        if (Input.GetKeyDown(KeyCode.LeftShift)) { ChangeTeam(); }
+       
+    }
+    public void ChangeFocus()
+    {
+        if (selected != null) { selected.ToggleFocus(); }
+        soldierInList++;
+        if (activeTeam == 0)
+        {
+
+            
+            
+            if (soldierInList >= team0.Count) { soldierInList = 0; }
+            team0[soldierInList].ToggleFocus();
+            selected = team0[soldierInList];
+
+        }
+        else {
+           
+           
+            if (soldierInList >= team1.Count) { soldierInList = 0; }
+            team1[soldierInList].ToggleFocus();
+            selected = team1[soldierInList];
+        }
+        cam.GetComponent<ThirdPersonOrbitCam>().player = selected.transform;
+    }
+
+    public void ChangeTeam()
+    {
+        if (selected != null) { selected.ToggleFocus(); }
+       
+        soldierInList = 0;
+        if (activeTeam == 0)
+        {
+            activeTeam = 1;
+
+
+          
+            team1[soldierInList].ToggleFocus();
+            selected = team1[soldierInList];
+
+        }
+        else
+        {
+
+            activeTeam = 0;
+           
+            team0[soldierInList].ToggleFocus();
+            selected = team0[soldierInList];
+        }
+        cam.GetComponent<ThirdPersonOrbitCam>().player = selected.transform;
+
+    }
+
+}
---
+++ turnbased/Assets/_scripts/Soldier.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool focused;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleFocus()
+    {
+        if (focused == true) {
+            focused = false;
+           
+            //GetComponent<Animator>().speed = 0;
+            GetComponent<Animator>().Play("Change");
+            GetComponent<BasicBehaviour>().enabled = false ;
+            GetComponent<MoveBehaviour>().enabled = false;
+            GetComponent<AimBehaviour>().enabled = false;
+            GetComponent<FlyBehaviour>().enabled = false;
+        }
+        else { focused = true;
+            GetComponent<Animator>().Play("Idle");
+            GetComponent<BasicBehaviour>().enabled = true;
+            GetComponent<MoveBehaviour>().enabled = true;
+            GetComponent<AimBehaviour>().enabled = true;
+            GetComponent<FlyBehaviour>().enabled = true;
+        }
+    }
+
+}
